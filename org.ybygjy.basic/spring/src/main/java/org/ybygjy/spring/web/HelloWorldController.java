4a31ca07038f7577b1708bec4c025a77350bef79
ybygjy <ybygjy@gmail.com>
2016-09-20 14:31:05 +0800

    1.spring mvc

--- org.ybygjy.basic/spring/src/main/java/org/ybygjy/spring/web/HelloWorldController.java
+++ org.ybygjy.basic/spring/src/main/java/org/ybygjy/spring/web/HelloWorldController.java
@@ -1,22 +1,19 @@
 package org.ybygjy.spring.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Service Controller
  * @author WangYanCheng
  * @version 2016年9月19日
  */
-public class HelloWorldController implements org.springframework.web.servlet.mvc.Controller {
-
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("message", "HelloWorld");
-        modelAndView.setViewName("HelloWorld");
-        return modelAndView;
+@Controller
+@RequestMapping("/helloWorld")
+public class HelloWorldController {
+    @RequestMapping("/sayHello")
+    public String sayHello() {
+        System.out.println("HelloWorld");
+        return "HelloWorld";
     }
 }
