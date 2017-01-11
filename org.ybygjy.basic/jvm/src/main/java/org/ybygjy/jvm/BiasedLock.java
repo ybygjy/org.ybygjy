36a03d28e7f0fe13ebdc37d232516edb4b21f37d
yancheng.wyc@alibaba-inc.com <yancheng.wyc@alibaba-inc.com>
2017-01-12 01:26:11 +0800

    数据修复工具

--- org.ybygjy.basic/jvm/src/main/java/org/ybygjy/jvm/BiasedLock.java
+++ org.ybygjy.basic/jvm/src/main/java/org/ybygjy/jvm/BiasedLock.java
@@ -6,6 +6,7 @@ import java.util.Vector;
 /**
  * 偏向锁
  * 偏向锁的核心思想是如果程序没有竞争则取消之前已经取得锁的线程同步操作，也就是说某一锁被线程获取后，进入偏向锁模式，当线程再进入请求这个锁时就无需要再进行相关的操作了
+ * <p>-XX:-UseBiasedLocking -XX:BiasedLockingStartupDelay=0 -client</p>
  * Created by leye on 2016/12/26.
  */
 public class BiasedLock {
