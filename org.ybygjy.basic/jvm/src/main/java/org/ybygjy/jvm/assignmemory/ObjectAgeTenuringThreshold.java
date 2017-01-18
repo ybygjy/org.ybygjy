78d0ae30ed4774e36d1d7aa2781dc82dc416a4e9
ybygjy <ybygjy@gmail.com>
2017-01-19 00:43:16 +0800

    test JVM PretenereSizeThreshold

--- org.ybygjy.basic/jvm/src/main/java/org/ybygjy/jvm/assignmemory/ObjectAgeTenuringThreshold.java
+++ org.ybygjy.basic/jvm/src/main/java/org/ybygjy/jvm/assignmemory/ObjectAgeTenuringThreshold.java
@@ -3,6 +3,7 @@ package org.ybygjy.jvm.assignmemory;
 /**
  * 动态对象年龄判定
  * -如果在Survivor空间中相同年龄所有对象大小的总和大于Survivor空间的一半，年龄大于或等于该年龄的对象就可以直接进入老年代，无须等到MaxTenuringThreshold中要求的年龄
+ * -java -cp ./jvm-1.0.0.jar -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:+UseSerialGC -XX:SurvivorRatio=8 -XX:+PrintTenuringDistribution -XX:MaxTenuringThreshold=15 org.ybygjy.jvm.assignmemory.ObjectAgeTenuringThreshold
  * Created by leye on 2017/1/19.
  */
 public class ObjectAgeTenuringThreshold {
