a5e4148a340e5f0c1fa39948863a0a75983120b7
ybygjy@gmail.com <ybygjy@gmail.com>
2017-02-19 00:27:13 +0800

    KMP Algorithm

--- org.ybygjy.basic/algorithm/src/main/java/org/ybygjy/basic/algorithm/str/KMPPractice.java
+++ org.ybygjy.basic/algorithm/src/main/java/org/ybygjy/basic/algorithm/str/KMPPractice.java
@@ -5,31 +5,7 @@ package org.ybygjy.basic.algorithm.str;
  * Created by leye on 2017/2/17.
  */
 public class KMPPractice {
-    /**
-     * 穷举法
-     * @param t t
-     * @param p p
-     * @return rtnInt -1/rtn
-     */
-    public static int strMatch(String t, String p) {
-        for (int i = 0; i < t.length(); i++) {
-            if (i + p.length() > t.length()) {
-                break;
-            }
-            boolean match = true;
-            for (int j = 0; j < p.length(); j++) {
-                if (t.charAt(i + j) != p.charAt(j)) {
-                    match = false;
-                    break;
-                }
-            }
-            if (match) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    class KMPArithmetic {
+    class KMPAlgorithm {
         /**
          * Brute-Force
          * @param str str
@@ -105,12 +81,13 @@ public class KMPPractice {
             }
         }
     }
-    public int kmpWork(String str, String sub) {
-        
+
+    public static int kmpPower(String str, String sub) {
+        return new KMPPractice().new KMPAlgorithm().kmp(str, sub);
     }
     public static void main(String[] args) {
         String t = "ABNCDFDADFDSA";
         String p = "FDADF";
-        System.out.println(KMPPractice.strMatch(t, p));
+        System.out.println(KMPPractice.kmpPower(t, p));
     }
 }
