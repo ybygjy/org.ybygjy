25ba7c5b1294c377bdf10c0aabdb9031b840a766
ybygjy@gmail.com <ybygjy@gmail.com>
2017-11-07 21:08:55 +0800

    同步代码

--- org.ybygjy.basic/basic/src/main/java/org/ybygjy/basic/basic/poi/HelloPOI.java
+++ org.ybygjy.basic/basic/src/main/java/org/ybygjy/basic/basic/poi/HelloPOI.java
@@ -1,6 +1,7 @@
 package org.ybygjy.basic.basic.poi;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -11,8 +12,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.text.DecimalFormat;
 import java.util.Iterator;
 
 /**
@@ -20,32 +21,39 @@ import java.util.Iterator;
  */
 public class HelloPOI {
     public static void main(String[] args) {
-        File file = new File("/Users/leye/tmp/cainiao_yz_201705.xlsx");
-        Path outputFilePath = FileSystems.getDefault().getPath("/Users/leye/tmp/", "cainiao_yz_201705_utf8.txt");
+        File file = new File("/Users/leye/1002_project/1015_双11/101_业务数据/20171107_社区二维码站点明细.xlsx");
+        File outputFile = FileSystems.getDefault().getPath("/Users/leye/tmp/", "cainiao_yz_201705_utf8.txt").toFile();
         BufferedWriter bufferedWriter = null;
         FileInputStream fileInputStream = null;
         try {
+            if (outputFile.canWrite()) {
+                System.out.println("删除输出文件：" + outputFile.delete() + "，path:" + outputFile);
+            }
             fileInputStream = new FileInputStream(file);
             XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
-            int numOfSheets = xssfWorkbook.getNumberOfSheets();
-            System.out.println("number of sheet=>" + numOfSheets + ":" + xssfWorkbook.getSheetName(0));
-            bufferedWriter = Files.newBufferedWriter(outputFilePath, StandardCharsets.UTF_8, StandardOpenOption.CREATE_NEW);
+            bufferedWriter = Files.newBufferedWriter(outputFile.toPath(), StandardCharsets.UTF_8, StandardOpenOption.CREATE);
             final BufferedWriter tmpBuffWriter = bufferedWriter;
             xssfWorkbook.forEach(sheetObj ->{
+                String sheetName = sheetObj.getSheetName();
                 System.out.println("getPhysicalNumberOfRows:" + sheetObj.getPhysicalNumberOfRows());
                 Iterator<Row> rowIterator = sheetObj.rowIterator();
                 Row row = rowIterator.next();
                 int cellNums = row.getPhysicalNumberOfCells();
                 while(rowIterator.hasNext()) {
                     row = rowIterator.next();
-                    StringBuilder sbud = new StringBuilder();
-                    for (int i = 0; i < cellNums; i++) {
-                        Cell cell = row.getCell(i, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-                        String cellVal = cell.getStringCellValue();
-                        cellVal = (null == cellVal || "".equals(cellVal)) ? "1970-01-01 00:00:00" : cellVal;
-                        sbud.append(cellVal).append(",");
+                    StringBuilder sbud = new StringBuilder(sheetName);
+                    for (int j = 0; j < cellNums; j++) {
+                        Cell cell = row.getCell(j, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                        CellType cellType = cell.getCellTypeEnum();
+                        String cellVal = "";
+                        if (CellType.NUMERIC == cellType) {
+                            cellVal = new DecimalFormat("0").format(cell.getNumericCellValue());
+                        } else {
+                            cellVal = String.valueOf(cell.getStringCellValue());
+                        }
+                        sbud.append(",").append(cellVal.trim());
                     }
-                    sbud.setLength(sbud.length() - 1);
+                    //sbud.setLength(sbud.length() - 1);
                     try {
                         tmpBuffWriter.write(sbud.toString());
                         tmpBuffWriter.newLine();
